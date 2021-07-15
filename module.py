7fa73dba5e4b257fc1d9288df730bc5a631837fd
06hong <86993454+06hong@users.noreply.github.com>
2021-07-14 21:03:02 -0400

    Add files via upload

---
+++ module.py
@@ -0,0 +1,13 @@
+def printName(name):
+    print(f"Hello Mr/Ms {name}...we've been waiting for you!")
+    
+     # l X w = area 
+def formula_area(length, width):
+    area = length*width 
+    return area 
+
+from math import pi
+def circumference(radius):
+    return (2* pi *radius)
+  
+    
